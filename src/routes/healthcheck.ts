3cc1b3018d20e46f5170b4f97d5460f77ebf23e5
Tegar Aditya <tegaraditya53@gmail.com>
2024-09-30 11:37:48 +0700

    changes on API

--- src/routes/healthcheck.ts
+++ src/routes/healthcheck.ts
@@ -1,10 +1,35 @@
+import { db } from "@/libs/database"
 import Elysia from "elysia"
 import { ip } from "elysia-ip"
 
-export const HealthCheckRouter = new Elysia().use(ip()).get("/", (req) => {
-    return {
-        client: req.ip,
-        status: "ok",
-        message: "Elysia is running",
+export const HealthCheckRouter = new Elysia().use(ip()).get(
+    "/",
+    (req) => {
+        return {
+            client: req.ip,
+            status: statusBuilder().then((data) => data),
+            message: "Elysia is running",
+        }
+    },
+    {
+        detail: {
+            summary: "Healthcheck",
+            tags: ["Healthcheck"],
+        },
     }
-})
+)
+
+const statusBuilder = async () => {
+    try {
+        const dbCheck = await db.$queryRaw`SELECT 1`
+        return {
+            db: "OK",
+            message: dbCheck,
+        }
+    } catch (error) {
+        return {
+            db: "Error",
+            message: error,
+        }
+    }
+}
