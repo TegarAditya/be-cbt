1247be7a96abb1b1fb064f6ba0f72573f6d9717f
Tegar Aditya <tegaraditya53@gmail.com>
2024-10-02 19:30:37 +0700

    refactor: Update mapel validations and add default values
    - Update listAllMapelQueryValidation to include default values for type, limit, page, and search
    - Add getMapelByIdQueryValidation with default value for type and description
    - Update getMapelByIdParamsValidation to include a description for the id parameter

--- src/validations/mapel.ts
+++ src/validations/mapel.ts
@@ -1,9 +1,40 @@
 import { t } from "elysia"
 
 export const listAllMapelQueryValidation = t.Object({
-    type: t.String({ enum: ["cbt", "pts", "pas", "all"] }),
+    type: t.String({
+        enum: ["cbt", "pts", "pas", "all"],
+        default: "cbt",
+        description: "Type of mapel to be fetched",
+    }),
+    limit: t.Optional(
+        t.Numeric({
+            default: 25,
+            maximum: 100,
+        })
+    ),
+    page: t.Optional(
+        t.Numeric({
+            default: 1,
+        })
+    ),
+    search: t.Optional(
+        t.String({
+            default: "",
+            examples: ["MTK", "IPA", "BJTI"],
+        })
+    ),
+})
+
+export const getMapelByIdQueryValidation = t.Object({
+    type: t.String({
+        enum: ["id", "id_ujian", "id_referrer"],
+        default: "id",
+        description: "Type of mapel_id to be fetched",
+    }),
 })
 
 export const getMapelByIdParamsValidation = t.Object({
-    id: t.Numeric(),
+    id: t.Numeric({
+        description: "ID of the mapel",
+    }),
 })
