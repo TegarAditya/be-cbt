72afa708424117652dd0e4148e0f52af2377d913
Tegar Aditya <tegaraditya53@gmail.com>
2024-10-10 14:08:29 +0700

    docs: adding `decodeString` documentation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,42 +6,89 @@ import { PrismaClient, soal } from "@prisma/client"
 /** UTILITY FUNCTIONS */
 
 /**
- * Decodes a string containing Unicode escape sequences (e.g., \uXXXX) into their corresponding characters.
+ * Decodes a given string by converting Unicode sequences and common HTML entities
+ * to their respective characters.
  *
- * @param str - The string containing Unicode escape sequences to decode.
- * @returns The decoded string with Unicode escape sequences replaced by their corresponding characters.
+ * @param str - The string to decode.
+ * @returns The decoded string.
+ *
+ * @example
+ * ```typescript
+ * const encodedStr = "Hello &amp; welcome to the world of \\u003CTypeScript\\u003E!";
+ * const decodedStr = decodeString(encodedStr);
+ * console.log(decodedStr); // "Hello & welcome to the world of <TypeScript>!"
+ * ```
  */
-export const decodeUnicode = (str: string): string => {
-    return str.replace(/\\u([0-9A-Fa-f]{4})/g, (match, grp) => {
+async function decodeString(str: string): Promise<string> {
+    if (!str) return ""
+
+    let decodedStr = str.replace(/\\u([0-9A-Fa-f]{4})/g, (match, grp) => {
         return String.fromCharCode(parseInt(grp, 16))
     })
+
+    // Replace the image URL with base64 if there are any images
+    // decodedStr = await replaceImageWithBase64(decodedStr)
+
+    return decodedStr
+}
+
+async function replaceImageWithBase64(htmlString: string): Promise<string> {
+    const urlRegex = /<img.*?src="(.*?)"/
+    const match = htmlString.match(urlRegex)
+    if (!match || !match[1]) {
+        return htmlString
+    }
+
+    const imageUrl = match[1] // The URL of the image
+
+    const base64Image = await convertImageToBase64(imageUrl)
+
+    const updatedHtmlString = htmlString.replace(imageUrl, base64Image)
+
+    return updatedHtmlString
+}
+
+async function convertImageToBase64(url: string): Promise<string> {
+    const response = await fetch(url)
+    const arrayBuffer = await response.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    const base64Image = buffer.toString("base64")
+    const mimeType = response.headers.get("Content-Type") || "image/png"
+    return `data:${mimeType};base64,${base64Image}`
 }
 
 /**
  * Transforms an array of `soal` objects by mapping each item to a new structure.
  *
  * @param soal - An array of `soal` objects to be transformed.
- * @returns An array of transformed `soal` objects with specific properties.
+ * @returns A Promise that resolves to an array of transformed `soal` objects.
  */
-export const transformSoalData = (soal: soal[]) => {
-    return soal.map((item) => ({
-        nomor: item.nomor,
-        soal: decodeUnicode(item.soal as string),
-        jawaban: `pil${decodeUnicode(item.jawaban as string)}`,
-        pembahasan: decodeUnicode(item.pembahasan as string),
-        pilA: decodeUnicode(item.pilA as string),
-        pilB: decodeUnicode(item.pilB as string),
-        pilC: decodeUnicode(item.pilC as string),
-        pilD: decodeUnicode(item.pilD as string),
-        pilE: decodeUnicode(item.pilE as string),
-        file: decodeUnicode(item.file as string),
-        file1: decodeUnicode(item.file1 as string),
-        fileA: decodeUnicode(item.fileA as string),
-        fileB: decodeUnicode(item.fileB as string),
-        fileC: decodeUnicode(item.fileC as string),
-        fileD: decodeUnicode(item.fileD as string),
-        fileE: decodeUnicode(item.fileE as string),
-    }))
+export const transformSoalData = async (soal: soal[]): Promise<any[]> => {
+    const transformedSoal = []
+
+    // Use for...of loop to handle async/await properly
+    for (const item of soal) {
+        transformedSoal.push({
+            nomor: item.nomor,
+            soal: await decodeString(item.soal as string),
+            jawaban: `pil${await decodeString(item.jawaban as string)}`,
+            pembahasan: await decodeString(item.pembahasan as string),
+            pilA: await decodeString(item.pilA as string),
+            pilB: await decodeString(item.pilB as string),
+            pilC: await decodeString(item.pilC as string),
+            pilD: await decodeString(item.pilD as string),
+            pilE: await decodeString(item.pilE as string),
+            file: await decodeString(item.file as string),
+            file1: await decodeString(item.file1 as string),
+            fileA: await decodeString(item.fileA as string),
+            fileB: await decodeString(item.fileB as string),
+            fileC: await decodeString(item.fileC as string),
+            fileD: await decodeString(item.fileD as string),
+            fileE: await decodeString(item.fileE as string),
+        })
+    }
+
+    return transformedSoal
 }
 
 /**
