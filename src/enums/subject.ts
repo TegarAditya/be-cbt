9aedd72e123f78441399dfc77722122c951044eb
Tegar Aditya <tegaraditya53@gmail.com>
2024-10-14 14:22:30 +0700

    Refactor subject.ts to use getSubjectEnumValues function

--- src/enums/subject.ts
+++ src/enums/subject.ts
@@ -2,33 +2,33 @@
  * Enum-like object representing various subjects and their associated codes.
  * Each subject is mapped to an array of strings, where the first element is the primary identifier
  * and the subsequent elements are alternative codes or abbreviations.
- * 
+ *
  * @constant
  * @type {object}
- * 
+ *
  * @property {string[]} BahasaIndonesia - Language subject: Bahasa Indonesia
  * @property {string[]} BahasaInggris - Language subject: Bahasa Inggris
  * @property {string[]} BahasaArab - Language subject: Bahasa Arab
  * @property {string[]} BahasaJawaTengah - Language subject: Bahasa Jawa Tengah
  * @property {string[]} BahasaJawaTimur - Language subject: Bahasa Jawa Timur
  * @property {string[]} BahasaSunda - Language subject: Bahasa Sunda
- * 
+ *
  * @property {string[]} AkidahAkhlak - Religious study: Akidah Akhlak
  * @property {string[]} Fikih - Religious study: Fikih
  * @property {string[]} PAIBP - Religious study: Pendidikan Agama Islam dan Budi Pekerti
  * @property {string[]} Qurdis - Religious study: Quran Hadis
  * @property {string[]} SKI - Religious study: Sejarah Kebudayaan Islam
- * 
+ *
  * @property {string[]} Biologi - Science subject: Biologi
  * @property {string[]} Fisika - Science subject: Fisika
  * @property {string[]} Kimia - Science subject: Kimia
  * @property {string[]} IPA - Science subject: Ilmu Pengetahuan Alam
  * @property {string[]} IPAS - Science subject: Ilmu Pengetahuan Alam dan Sosial
  * @property {string[]} Informatika - Science subject: Informatika
- * 
+ *
  * @property {string[]} Matematika - Mathematics subject: Matematika
  * @property {string[]} MatematikaPeminatan - Mathematics subject: Matematika Peminatan
- * 
+ *
  * @property {string[]} IPS - Social science subject: Ilmu Pengetahuan Sosial
  * @property {string[]} Geografi - Social science subject: Geografi
  * @property {string[]} Ekonomi - Social science subject: Ekonomi
@@ -36,17 +36,17 @@
  * @property {string[]} Sosiologi - Social science subject: Sosiologi
  * @property {string[]} Sejarah - Social science subject: Sejarah
  * @property {string[]} SejarahIndonesia - Social science subject: Sejarah Indonesia
- * 
+ *
  * @property {string[]} SeniBudaya - Art subject: Seni Budaya
  * @property {string[]} SeniMusik - Art subject: Seni Musik
  * @property {string[]} SeniRupa - Art subject: Seni Rupa
- * 
+ *
  * @property {string[]} Penjas - Physical education subject: Pendidikan Jasmani
- * 
+ *
  * @property {string[]} PKWU - Vocational subject: Prakarya dan Kewirausahaan
  * @property {string[]} Prakarya - Vocational subject: Prakarya
  * @property {string[]} PendidikanPancasila - Vocational subject: Pendidikan Pancasila dan Kewarganegaraan
- * 
+ *
  * @property {string[]} BK - Miscellaneous subject: Bimbingan Konseling
  * @property {string[]} BTQ - Miscellaneous subject: Baca Tulis Quran
  */
@@ -98,7 +98,7 @@ export const SubjectEnum: object = {
     // Vocational and practical subjects
     PKWU: ["pkwu", "PKWU"],
     Prakarya: ["prakarya", "PRA", "PRAKARYA"],
-    PendidikanPancasila: ["pendidikan-pancasila", "PKN", "PP", "PENDIDIKANPANCASILA"],
+    PendidikanPancasila: ["pendidikan-pancasila", "PKN", "PP", "PANCASILA"],
 
     // Miscellaneous
     BK: ["bk", "BK"],
@@ -113,6 +113,17 @@ export type SubjectType = keyof typeof SubjectEnum
  * @param subject - The subject type for which to get the enum values.
  * @returns An array of strings representing the enum values for the specified subject.
  */
-export const getEnumValues = (subject: SubjectType): readonly string[] => {
+export const getSubjectEnumValues = (
+    subject: SubjectType
+): readonly string[] => {
     return SubjectEnum[subject]
+}
+
+/**
+ * Retrieves the keys of the SubjectEnum as an array of SubjectType.
+ *
+ * @returns An array containing the keys of the SubjectEnum.
+ */
+export const getSubjectEnumKeys = (): SubjectType[] => {
+    return Object.keys(SubjectEnum) as SubjectType[]
 }
